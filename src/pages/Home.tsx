32617cda9399fb2c0d8b40bc99432629d5456c75
abneribeiro <eliasabner38@gmail.com>
2024-10-02 18:27:33 +0100

    add: refactoring code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
-      <h1 className="text-3xl font-bold underline text-red-900">
-        
-      </h1>
+    <div className="bg-gray-950">
+
+    </div>
     )
   }
