10d65785c9604442798db41fc581b7366448c32e
abneribeiro <eliasabner38@gmail.com>
2024-10-02 18:27:10 +0100

    add: IconBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import IconBar from "./components/Footer";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <div>
+    <div className="">
+      <Home/>
       <IconBar/>
     </div>
   )
