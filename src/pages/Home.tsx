71edfc74c8dc51f066d8abc9a7c52c7bee0c8c16
abneribeiro <eliasabner38@gmail.com>
2024-10-03 10:24:42 +0100

    add: home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
     return (
-    <div className="bg-gray-950">
-
+    <div className="w-full h-screen bg-gray-300 flex justify-center items-center">
+      <div>
+        <h2 className="text-4xl">Abner Ribeiro</h2>
+        <p>Software Developer</p>
+      </div>
     </div>
     )
   }
